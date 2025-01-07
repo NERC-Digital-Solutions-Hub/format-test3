b4a15210fcf7edcd5a2633a3e95e5257f6a3ff1d
WilliamL-R <w.loveridge1@gmail.com>
2025-01-07 16:12:50 +0000

    feat: added c# project for dotnet format tests

---
+++ DotNet/TestFormatProject/TestFormatProject/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace TestFormatProject {
+    internal class Program {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
